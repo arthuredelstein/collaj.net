7bd46601d56d2b45a221f710fc9abbb05f143864
Arthur Edelstein <arthuredelstein@gmail.com>
2012-01-26 22:08:31 -0800

    document solr functions

--- co/src/co/solr.clj
+++ co/src/co/solr.clj
@@ -9,33 +9,42 @@
 (def solr-query-address (str solr-address "select/"))
 
 (defn solr-update
-  "Post data to the local solr server."
+  "Post data to the local solr database."
   [data]
   (client/post solr-json-update-address {:body (json-str data)}))
 
-(defn solr-add-doc [doc-data]
-  "Add a single document to a solr database."
+(defn solr-add-doc
+  "Add a single document to the solr database."
+  [doc-data]
   (solr-update {:add {:doc doc-data}}))
 
-(defn solr-add-docs [docs]
+(defn solr-add-docs 
+  "Add multiple documents to the solr database."
+  [docs]
   (doall (map solr-add-doc docs)))
 
-(defn solr-delete-all []
+(defn solr-delete-all
   "Remove all documents from solr database."
+  []
   (solr-update {:delete {:query "*:*"}}))
 
-(defn solr-commit []
+(defn solr-commit
   "Commit changes to solr database."
+  []
   (solr-update {:commit {}}))
 
-(defn solr-query [params]
+(defn solr-query
+  "Read data from a solr database"
+  [params]
   (let [full-params (merge {:wt "json"} params)]
     (-> (client/get solr-query-address
                 {:query-params full-params})
         :body
         read-json)))
 
-(defn solr-get-docs [params]
+(defn solr-get-docs
+  "Get document data from a solr query"
+  [params]
   (-> params solr-query :response :docs))
               
 ;; tests
