fb65192ace5000f7f7a410711206f7c733503699
Arthur <arthur@li86-155.members.linode.com>
2012-03-06 09:08:40 +0000

    fix incorrect main namespace

--- index/project.clj
+++ index/project.clj
@@ -1,6 +1,6 @@
 (defproject index "0.0.1"
   :description "FIXME: write"
-  :main site.core
+  :main co.core
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [local-file "0.1.0"]
                  [clj-http "0.2.7"]
