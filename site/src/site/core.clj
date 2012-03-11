3d833df0e5b858b933ecdf856d72009de496d730
Arthur Edelstein <arthuredelstein@gmail.com>
2012-03-11 01:35:14 -0800

    make slightly readable

--- site/src/site/core.clj
+++ site/src/site/core.clj
@@ -1,9 +1,11 @@
 (ns site.core
   (:require [solrclient (core :as solr)])
-  (:use compojure.core,
-        ring.adapter.jetty)
+  (:use compojure.core
+        ring.adapter.jetty
+        hiccup.core)
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            ))
 
 (defn sanitize [s]
   (.replaceAll s "[!@#$%\\^&\\*()_\\+={}\\|\\\\\\[\\]:;\\\"\\'\\<\\>\\.\\,\\?\\/\\`\\~]" " "))
@@ -11,7 +13,7 @@
 (defn search [text]
   (solr/query
     {:q (sanitize text)
-     :rows 10
+     :rows 20
      :fl "score,name,doc,arglists,ns,source,var-type,artifact"
      :group true
      :group.field "doc"
@@ -23,20 +25,34 @@
   (let [groups (-> results :grouped :doc :groups)]
     (println "Matches:" (count groups))
     (doseq [group groups]
+      (println "<hr>")
       (let [{:keys [name arglists ns doc var-type source artifact]}
             (-> group :doclist :docs first)]
         (println (str name " (" ns ") -- " artifact))
         (when arglists (println arglists))
-        (println doc "\n")
-        (println (if doc (.replace source doc "...") source) "\n")))))
+        (println "\n" doc "\n")
+        (println "\nSource:\n\n" (if doc (.replace source doc "...") source) "\n")))))
+
+(defhtml search-page [last-query results]
+ [:html
+         [:header
+          [:title "collaj: clojure function search"]]
+         [:body
+          [:form {:action "/"}
+           [:input {:type "text" :name "q" :value last-query}]
+           [:input {:type "submit" :value "Search"}]
+         ]
+          [:pre results]]])
 
 (defroutes main-routes
-  (GET "/" [] "Hi!")
-  (GET "/data/:term" [term] (pr-str (search term)))
+  (GET "/" [q] (search-page q (with-out-str (display (search q)))))
+  (GET "/data/:term" [term] (pr-str (escape-html (search term))))
   (GET "/:term" [term] (str "<html><body><pre>"
                             (with-out-str (display (search term)))
                             "</pre></body></html>"))
   (route/not-found "<h1>Page not found</h1>"))
+
+(handler/api routes)
 
 (def app (handler/site main-routes))
 
