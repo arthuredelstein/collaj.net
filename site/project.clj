589aed775492d191a47bcf50c419572e4f2ba19d
Arthur Edelstein <arthuredelstein@gmail.com>
2012-09-05 00:02:53 -0700

    bring project.clj file up to date for lein2

--- site/project.clj
+++ site/project.clj
@@ -1,17 +1,19 @@
 (defproject site "0.0.1"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.0"]
+                 [compojure "1.1.3"]
                  [solrclient "0.0.2"]
                  [hiccup "1.0.0"]
+                 ;[fetch "0.1.0-alpha2"]
                  ;[crate "0.2.0-alpha4"]
                  ;[moumar/domina "1.0.0-beta5"]
-                 [org.clojure/clojurescript "0.0-1424"] ;1236, 1424
+                 ;[org.clojure/clojurescript "0.0-1443"] ;1236, 1424, 1443
                  ]
-            :dev-dependencies [[lein-cljsbuild "0.2.1"]    
-                     [lein-ring "0.7.1"]
-                     [lein-git-deps "0.0.1-SNAPSHOT"]]
-  :git-dependencies [;["https://github.com/arthuredelstein/domina.git"]
+            :dev-dependencies    
+                     [[lein-git-deps "0.0.1-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "0.2.7"]
+            [lein-ring "0.7.5"]]
+  :git-dependencies [["https://github.com/arthuredelstein/domina.git"]
                      ["https://github.com/arthuredelstein/crate.git"]
                      ]
   :extra-classpath-dirs [".lein-git-deps/domina/src/"
